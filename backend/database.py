954b28980ea11790336570030aa6146c37326f3f
Omar Sayed <osayed908@gmail.com>
2026-05-10 21:31:24 +0300

    Creating the Database

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,14 @@
-# backend/database.py
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("Database.db")
+    
+    with open("schema.sql", "r") as f:
+        conn.executescript(f.read())
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
+    print("Database created successfully.")
